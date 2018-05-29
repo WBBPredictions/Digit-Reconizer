735c8181cdd557f46b58fe5f030b3078a7c168ff
Kevin Wachs <kwachs21@gmail.com>
2018-05-29 10:18:08 -0700

    plot after mtrain added

--- Wbbp digit.R
+++ Wbbp digit.R
@@ -1,6 +1,13 @@
 digits=read.csv(file.choose(),header=T)
 nrow(digits) #42000
 ncol(digits) #785
+mtrain <- as.matrix(digits)
+mtrain <- as.matrix(c(mapply(OneOrZero, mtrain)))
+mtrain <- matrix(mtrain, nrow = nrow(digits), ncol = ncol(digits), byrow = FALSE)
+
+summtrain=apply(mtrain,2,sum)
+plot(1:784,as.vector(summtrain)[-1])
 
 sumcol=apply(digits,2,sum)
 plot(1:784,as.vector(sumcol)[-1])
+
