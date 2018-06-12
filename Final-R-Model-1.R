87da687a609ceb1ff8ecef29b70b1efca1c2b1c3
TravisBarton <travis.barton@sjsu.edu>
2018-06-11 22:53:25 -0700

    ITS THE FINAL COUNTDOWN!!

--- Final-R-Model-1.R
+++ Final-R-Model-1.R
@@ -1,7 +1,19 @@
+library(caret)
+library(class)
+library(beepr)
+library(readr)
+library(e1071)
+Mini_train <- read_csv("~/Desktop/Projects/Group/Mini_train.csv", col_types = cols(X1 = col_skip()))
+train <- Mini_train
+resp <- Mini_train$label
+preds <- Mini_train[,-1]
+
 The_Deal <- fuction(train, point, model)
 {
   # Fit the SVM, KNN, Random Forest (RF)
-  
+  fit.svm <- svm(label ~ ., data = train, type = "C-classification", kernel = "linear", cross = 5)
+  obj <- tune(svm, label ~ ., data = train, ranges = list(gamma = 2^(-1:1), cost = 2^(0:4)), tunecontrol = tune.control(sampling = "fix"))
+  summary(fit.svm)
   # Pull certainty from SVM, KNN and RF
   
   # Use those decisions to fit a logistic model (trust or not trust)
